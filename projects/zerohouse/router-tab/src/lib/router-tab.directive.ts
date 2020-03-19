b612f2540fb7754040b1d6a7d9b3720980cb044d
cativo <as.1337@outlook.com>
2020-03-19 13:51:09 +0400

    Updating MatTab import with angular 9 changes

--- projects/zerohouse/router-tab/src/lib/router-tab.directive.ts
+++ projects/zerohouse/router-tab/src/lib/router-tab.directive.ts
@@ -1,5 +1,5 @@
 import {Directive} from '@angular/core';
-import {MatTab} from '@angular/material';
+import { MatTab } from '@angular/material/tabs';
 import {RouterLink} from '@angular/router';
 
 @Directive({
