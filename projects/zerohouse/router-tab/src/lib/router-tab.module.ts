61584d8941b9ad90ec93f30f9b5a9d65a631a814
cativo <as.1337@outlook.com>
2020-05-06 15:07:19 +0400

    Supports Angular 9

--- projects/zerohouse/router-tab/src/lib/router-tab.module.ts
+++ projects/zerohouse/router-tab/src/lib/router-tab.module.ts
@@ -1,11 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterTab} from './router-tab.directive';
-import { MatTabGroup } from '@angular/material/tabs';
-import {RouterTabs} from './router-tabs.directive';
+import { NgModule } from "@angular/core";
+import { MatTabsModule } from "@angular/material/tabs";
+import { RouterTab } from "./router-tab.directive";
+import { RouterTabs } from "./router-tabs.directive";
 
 @NgModule({
   declarations: [RouterTab, RouterTabs],
-  exports: [MatTabsModule, RouterTabs, RouterTab]
+  exports: [MatTabsModule, RouterTabs, RouterTab],
 })
-export class RouterTabModule {
-}
+export class RouterTabModule {}
